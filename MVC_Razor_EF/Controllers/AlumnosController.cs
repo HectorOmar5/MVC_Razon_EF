c911543bc8a9e41098f26de0bbf332f7e43ba6c8
Hector Omar Nava Glz <71728454+HectorOmar5@users.noreply.github.com>
2022-10-03 15:16:20 -0500

    Proyecto MVC con razon y EntityFramework

---
+++ MVC_Razor_EF/Controllers/AlumnosController.cs
@@ -0,0 +1,102 @@
+﻿using MVC_Razor_EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Razor_EF.Controllers
+{
+    public class AlumnosController : Controller
+    {
+
+        InstitutoTichEntities _BDContext = new InstitutoTichEntities();
+        List<Alumnos> _listAlumnos;
+        Alumnos _alumnos;
+
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+
+            _listAlumnos = _BDContext.Alumnos.ToList();
+
+            return View(_listAlumnos);
+        }
+
+        // GET: Alumnos/Details/5
+        public ActionResult Details(int id)
+        {
+            _alumnos = _BDContext.Alumnos.Find(id);
+            return View(_alumnos);
+        }
+
+        // GET: Alumnos/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: Alumnos/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Alumnos/Edit/5
+        public ActionResult Edit(int id)
+        {
+            //_alumnos = _BDContext.Alumnos.Find(id);
+
+            return View();
+            //return View();
+        }
+
+        // POST: Alumnos/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                //TODO: Add update logic here
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Alumnos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Alumnos/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
